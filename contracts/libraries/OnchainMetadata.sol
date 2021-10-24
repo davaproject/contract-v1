2ad338989b034dd1899c8058614077f6d39eb2c6
Dr.Keccak <drkeccak@protonmail.com>
2021-10-24 20:49:57 +0900

    feat: add onchain svg logic to getPFP() func

--- contracts/libraries/OnchainMetadata.sol
+++ contracts/libraries/OnchainMetadata.sol
@@ -3,7 +3,18 @@ pragma solidity >=0.8.0;
 pragma abicoder v2;
 
 library OnchainMetadata {
+    struct OnchainSVG {
+        string svg;
+        uint256 zIndex;
+    }
+
     function toMetadata(string memory svg)
+        internal
+        pure
+        returns (string memory)
+    {}
+
+    function compileImages(OnchainSVG[] memory svgs)
         internal
         pure
         returns (string memory)
