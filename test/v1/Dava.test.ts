fd15ba0244cbb2b5bead23f2c24c659f898b812a
Dr.Keccak <drkeccak@protonmail.com>
2021-10-26 22:29:25 +0900

    test: use fixtures for testing

--- test/v1/Dava.test.ts
+++ test/v1/Dava.test.ts
@@ -6,34 +6,25 @@ import {
   AvatarV1,
   AvatarV1__factory,
   AvatarV2Draft__factory,
-  BeaconProxy,
-  BeaconProxy__factory,
   Dava,
-  Dava__factory,
-  MinimalProxy__factory,
 } from "../../types";
 import { solidity } from "ethereum-waffle";
 import { constants } from "ethers";
+import { fixtures } from "../../scripts/utils/fixtures";
 
 chai.use(solidity);
 const { expect } = chai;
 
 describe("Dava", () => {
   let snapshot: string;
-  let minimalProxy: BeaconProxy;
   let avatarV1: AvatarV1;
   let dava: Dava;
   let [deployer, ...accounts]: SignerWithAddress[] = [];
   before(async () => {
     [deployer, ...accounts] = await ethers.getSigners();
-    const MinimalProxyContract = new MinimalProxy__factory(deployer);
-    const AvatarV1Contract = new AvatarV1__factory(deployer);
-    const DavaContract = new Dava__factory(deployer);
-    minimalProxy = await MinimalProxyContract.deploy();
-    dava = await DavaContract.deploy(minimalProxy.address);
-    // set avatar implementation
-    avatarV1 = await AvatarV1Contract.deploy();
-    await dava.connect(deployer).upgradeTo(avatarV1.address);
+    const contracts = await fixtures();
+    dava = contracts.dava;
+    avatarV1 = contracts.avatarV1;
   });
   beforeEach(async () => {
     snapshot = await ethers.provider.send("evm_snapshot", []);
