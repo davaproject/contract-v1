684bb4b284e45a19140586fd78f067ceaf9871dd
ggomma <ggomma@gmail.com>
2021-10-29 00:03:26 +0900

    fix: remove path from uri

--- contracts/libraries/ImageHost.sol
+++ contracts/libraries/ImageHost.sol
@@ -2,30 +2,17 @@
 pragma solidity >=0.8.0;
 pragma abicoder v2;
 
-contract ImageHost {
-    string public host;
-
+library ImageHost {
     struct Query {
         string key;
         string value;
     }
 
-    event SetHost(string host);
-
-    constructor(string memory host_) {
-        host = host_;
-    }
-
-    function getFullUri(string[] memory paths, Query[] memory queries)
-        public
-        view
+    function getFullUri(string memory host, Query[] memory queries)
+        internal
+        pure
         returns (string memory)
     {
-        string memory path;
-        for (uint256 i = 0; i < paths.length; i += 1) {
-            path = string(abi.encodePacked(path, "/", paths[i]));
-        }
-
         string memory queryString;
         for (uint256 i = 0; i < queries.length; i += 1) {
             if (i != 0) {
@@ -41,11 +28,6 @@ contract ImageHost {
             );
         }
 
-        return string(abi.encodePacked(host, path, "?", queryString));
-    }
-
-    function _setHost(string memory host_) internal virtual {
-        host = host_;
-        emit SetHost(host_);
+        return string(abi.encodePacked(host, "?", queryString));
     }
 }
