8df749f77b84d246b6bf075c06f6eb9b9763d3b4
ggomma <ggomma@gmail.com>
2021-11-03 11:11:18 +0900

    test: FrameCollection

---
+++ contracts/tests/TestFrameCollection.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0;
+
+import {FrameCollection} from "../libraries/FrameCollection.sol";
+
+contract TestFrameCollection is FrameCollection {}
