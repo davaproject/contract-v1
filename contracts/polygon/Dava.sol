b75a8afb447e9cf9120ebd14b2c373d9b09fe352
ggomma <ggomma@gmail.com>
2021-12-05 17:09:32 +0900

    feat: implement polygon Dava, DavaOfficial contract

---
+++ contracts/polygon/Dava.sol
@@ -0,0 +1,142 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0;
+pragma abicoder v2;
+
+import {IGatewayHandler} from "../interfaces/IGatewayHandler.sol";
+import {Dava} from "../Dava.sol";
+
+contract PolygonDava is Dava {
+    bytes32 public constant DEPOSITOR_ROLE = keccak256("DEPOSITOR_ROLE");
+
+    // limit batching of tokens due to gas limit restrictions
+    uint256 public constant BATCH_LIMIT = 20;
+
+    event WithdrawnBatch(address indexed user, uint256[] tokenIds);
+    event TransferWithMetadata(
+        address indexed from,
+        address indexed to,
+        uint256 indexed tokenId,
+        bytes metaData
+    );
+
+    constructor(
+        address minimalProxy_,
+        IGatewayHandler gatewayHandler_,
+        address childChainManager_
+    ) Dava(minimalProxy_, gatewayHandler_) {
+        _setupRole(DEPOSITOR_ROLE, childChainManager_);
+    }
+
+    function _msgSender() internal view override returns (address sender) {
+        return msg.sender;
+    }
+
+    /**
+     * @notice called when token is deposited on root chain
+     * @dev Should be callable only by ChildChainManager
+     * Should handle deposit by minting the required tokenId for user
+     * Make sure minting is done only by this function
+     * @param user user address for whom deposit is being done
+     * @param depositData abi encoded tokenId
+     */
+    function deposit(address user, bytes calldata depositData)
+        external
+        onlyRole(DEPOSITOR_ROLE)
+    {
+        // deposit single
+        if (depositData.length == 32) {
+            uint256 tokenId = abi.decode(depositData, (uint256));
+            _mintWithProxy(user, tokenId);
+
+            // deposit batch
+        } else {
+            uint256[] memory tokenIds = abi.decode(depositData, (uint256[]));
+            for (uint256 i; i < tokenIds.length; i += 1) {
+                _mintWithProxy(user, tokenIds[i]);
+            }
+        }
+    }
+
+    /**
+     * @notice called when user wants to withdraw token back to root chain
+     * @dev Should burn user's token. This transaction will be verified when exiting on root chain
+     * @param tokenId tokenId to withdraw
+     */
+    function withdraw(uint256 tokenId) external {
+        require(
+            _msgSender() == ownerOf(tokenId),
+            "PolygonDava: INVALID_TOKEN_OWNER"
+        );
+        _burn(tokenId);
+    }
+
+    /**
+     * @notice called when user wants to withdraw multiple tokens back to root chain
+     * @dev Should burn user's tokens. This transaction will be verified when exiting on root chain
+     * @param tokenIds tokenId list to withdraw
+     */
+    function withdrawBatch(uint256[] calldata tokenIds) external {
+        uint256 length = tokenIds.length;
+        require(length <= BATCH_LIMIT, "PolygonDava: EXCEEDS_BATCH_LIMIT");
+        for (uint256 i; i < length; i++) {
+            uint256 tokenId = tokenIds[i];
+            require(
+                _msgSender() == ownerOf(tokenId),
+                string(
+                    abi.encodePacked(
+                        "ChildERC721: INVALID_TOKEN_OWNER ",
+                        tokenId
+                    )
+                )
+            );
+            _burn(tokenId);
+        }
+        emit WithdrawnBatch(_msgSender(), tokenIds);
+    }
+
+    /**
+     * @notice called when user wants to withdraw token back to root chain with arbitrary metadata
+     * @dev Should handle withraw by burning user's token.
+     *
+     * This transaction will be verified when exiting on root chain
+     *
+     * @param tokenId tokenId to withdraw
+     */
+    function withdrawWithMetadata(uint256 tokenId) external {
+        require(
+            _msgSender() == ownerOf(tokenId),
+            "ChildERC721: INVALID_TOKEN_OWNER"
+        );
+
+        // Encoding metadata associated with tokenId & emitting event
+        emit TransferWithMetadata(
+            _msgSender(),
+            address(0),
+            tokenId,
+            this.encodeTokenMetadata(tokenId)
+        );
+
+        _burn(tokenId);
+    }
+
+    /**
+     * @notice This method is supposed to be called by client when withdrawing token with metadata
+     * and pass return value of this function as second paramter of `withdrawWithMetadata` method
+     *
+     * It can be overridden by clients to encode data in a different form, which needs to
+     * be decoded back by them correctly during exiting
+     *
+     * @param tokenId Token for which URI to be fetched
+     */
+    function encodeTokenMetadata(uint256 tokenId)
+        external
+        view
+        virtual
+        returns (bytes memory)
+    {
+        // You're always free to change this default implementation
+        // and pack more data in byte array which can be decoded back
+        // in L1
+        return abi.encode(tokenURI(tokenId));
+    }
+}
---
+++ contracts/polygon/DavaOfficial.sol
@@ -0,0 +1,71 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0;
+
+import {IGatewayHandler} from "../interfaces/IGatewayHandler.sol";
+import {PartCollection} from "../libraries/PartCollection.sol";
+
+contract PolygonDavaOfficial is PartCollection {
+    bytes32 public constant DEPOSITOR_ROLE = keccak256("DEPOSITOR_ROLE");
+
+    constructor(
+        IGatewayHandler gatewayHandler_,
+        address dava_,
+        address childChainManager_
+    ) PartCollection(gatewayHandler_, dava_) {
+        _setupRole(DEPOSITOR_ROLE, childChainManager_);
+    }
+
+    function _msgSender() internal view override returns (address sender) {
+        return msg.sender;
+    }
+
+    function name() public pure returns (string memory) {
+        return "polygon-dava-official";
+    }
+
+    /**
+     * @notice called when tokens are deposited on root chain
+     * @dev Should be callable only by ChildChainManager
+     * Should handle deposit by minting the required tokens for user
+     * Make sure minting is done only by this function
+     * @param user user address for whom deposit is being done
+     * @param depositData abi encoded ids array and amounts array
+     */
+    function deposit(address user, bytes calldata depositData)
+        external
+        onlyRole(DEPOSITOR_ROLE)
+    {
+        (
+            uint256[] memory ids,
+            uint256[] memory amounts,
+            bytes memory data
+        ) = abi.decode(depositData, (uint256[], uint256[], bytes));
+        require(
+            user != address(0x0),
+            "PolygonDavaOfficial: INVALID_DEPOSIT_USER"
+        );
+        _mintBatch(user, ids, amounts, data);
+    }
+
+    /**
+     * @notice called when user wants to withdraw single token back to root chain
+     * @dev Should burn user's tokens. This transaction will be verified when exiting on root chain
+     * @param id id to withdraw
+     * @param amount amount to withdraw
+     */
+    function withdrawSingle(uint256 id, uint256 amount) external {
+        _burn(_msgSender(), id, amount);
+    }
+
+    /**
+     * @notice called when user wants to batch withdraw tokens back to root chain
+     * @dev Should burn user's tokens. This transaction will be verified when exiting on root chain
+     * @param ids ids to withdraw
+     * @param amounts amounts to withdraw
+     */
+    function withdrawBatch(uint256[] calldata ids, uint256[] calldata amounts)
+        external
+    {
+        _burnBatch(_msgSender(), ids, amounts);
+    }
+}
