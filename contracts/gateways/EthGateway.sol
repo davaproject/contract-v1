83cfaeaff06624e6806d7f95500a7aa4f70da9d7
ggomma <ggomma@gmail.com>
2022-02-02 11:12:17 +0900

    feat: simplify eth gateway system to reduce gas cost

--- contracts/gateways/EthGateway.sol
+++ contracts/gateways/EthGateway.sol
@@ -4,91 +4,63 @@ pragma abicoder v2;
 
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-import {IAvatar, Part} from "../interfaces/IAvatar.sol";
-import {IDava} from "../interfaces/IDava.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {ERC1155Receiver, ERC1155Holder, IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
-contract EthGateway is AccessControl {
-    using EnumerableSet for EnumerableSet.AddressSet;
-
+contract EthGateway is AccessControl, ERC1155Holder {
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
-    IDava public immutable dava;
-    EnumerableSet.AddressSet private erc1155List;
+    IERC721 public immutable dava;
+    IERC1155 public immutable parts;
 
-    event Locked(
+    event DavaLocked(address indexed requester, uint256[] ids);
+
+    event PartsLocked(
         address indexed requester,
-        bytes32[] data,
-        uint256 timestamp,
-        bytes32 dataHash
+        uint256[] ids,
+        uint256[] amounts
     );
 
-    constructor(IDava dava_) {
+    constructor(IERC721 dava_, IERC1155 parts_) {
         dava = dava_;
+        parts = parts_;
 
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(MANAGER_ROLE, msg.sender);
         _setRoleAdmin(MANAGER_ROLE, DEFAULT_ADMIN_ROLE);
     }
 
-    function register1155(address erc1155) external onlyRole(MANAGER_ROLE) {
-        erc1155List.add(erc1155);
-    }
-
-    function batchReceiveAvatar(uint48[] calldata tokenIds) external {
-        bytes32[] memory data;
-
+    function batchLockAvatars(address requester, uint256[] calldata tokenIds)
+        external
+    {
         for (uint256 i = 0; i < tokenIds.length; i += 1) {
-            dava.transferFrom(msg.sender, address(this), tokenIds[i]);
-            data[i] = encodeData(address(dava), tokenIds[i], 1);
+            dava.transferFrom(requester, address(this), tokenIds[i]);
         }
-        emit Locked(
-            msg.sender,
-            data,
-            block.timestamp,
-            hashData(msg.sender, data, block.timestamp)
-        );
+        emit DavaLocked(requester, tokenIds);
     }
 
-    function batchReceive1155(
-        IERC1155 erc1155,
+    function batchLockParts(
+        address requester,
         uint256[] calldata tokenIds,
         uint256[] calldata amounts
     ) external {
-        require(
-            erc1155List.contains(address(erc1155)),
-            "EthGateway: invalid erc1155 contract"
-        );
         require(
             tokenIds.length == amounts.length,
             "EthGateway: invalid tokenIds and amounts"
         );
 
-        bytes32[] memory data;
-
-        erc1155.safeBatchTransferFrom(
-            msg.sender,
+        parts.safeBatchTransferFrom(
+            requester,
             address(this),
             tokenIds,
             amounts,
             ""
         );
-        for (uint256 i = 0; i < tokenIds.length; i += 1) {
-            data[i] = encodeData(
-                address(erc1155),
-                uint48(tokenIds[i]),
-                uint48(amounts[i])
-            );
-        }
-        emit Locked(
-            msg.sender,
-            data,
-            block.timestamp,
-            hashData(msg.sender, data, block.timestamp)
-        );
+
+        emit PartsLocked(requester, tokenIds, amounts);
     }
 
-    function batchTransferAvatar(address receiver, uint256[] calldata tokenIds)
+    function batchTransferAvatars(address receiver, uint256[] calldata tokenIds)
         external
         onlyRole(MANAGER_ROLE)
     {
@@ -97,7 +69,7 @@ contract EthGateway is AccessControl {
         }
     }
 
-    function batchTransfer1155(
+    function batchTransferParts(
         address receiver,
         IERC1155 erc1155,
         uint256[] calldata tokenIds,
@@ -112,22 +84,14 @@ contract EthGateway is AccessControl {
         );
     }
 
-    function hashData(
-        address requester,
-        bytes32[] memory data,
-        uint256 timestamp
-    ) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(requester, data, timestamp));
-    }
-
-    function encodeData(
-        address addr,
-        uint48 tokenId,
-        uint48 amount
-    ) private pure returns (bytes32) {
-        assembly {
-            mstore(0x00, xor(xor(shl(96, addr), shl(48, tokenId)), amount))
-            return(0x00, 32)
-        }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC1155Receiver, AccessControl)
+        returns (bool)
+    {
+        return
+            interfaceId == type(IERC1155Receiver).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 }
